d2ab559237b2303ee9a413837017b0fb77b31c26
Diiipereira <dipereira@live.com>
2021-06-24 18:02:16 -0300

    Atualização nas autenticações do projeto

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -41,6 +41,11 @@ export function Home() {
       return;
     }
 
+    if (roomRef.val().endedAt) {
+      alert('Esta sala foi fechada.');
+      return;
+    }
+
     history.push(`/rooms/${roomCode}`);
   }
 
